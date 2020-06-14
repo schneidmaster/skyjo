1ccf119cf689bf9fc94aafa34bbf69c96383929b
Zach Schneider <zach@schneider.dev>
2020-06-13 20:50:02 -0500

    Avoid duplicate rounds

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,11 +1,12 @@
 class RoundsController < ApplicationController
   def create
     game = Game.find(params[:game_id])
+    game.lock!
     if game.rounds.none?
       game.rounds.create(round_number: 1)
       game.started!
       ActionCable.server.broadcast("games_#{game.token}", game)
-    else
+    elsif game.rounds.all?(&:finished?)
       game.rounds.create(round_number: game.rounds.count + 1)
       ActionCable.server.broadcast("rounds_#{game.token}", game.rounds)
     end
