eec668129ad0a25f2a34fcb091b58088c7c5cb4d
Zach Schneider <zach@schneider.dev>
2020-06-19 18:18:52 -0500

    Styling for homepage and lobby

--- app/javascript/packs/Lobby.jsx
+++ app/javascript/packs/Lobby.jsx
@@ -15,42 +15,53 @@ export default function Lobby({ game, participant, setParticipant }) {
   const [name, setName] = useState("");
 
   return (
-    <div>
-      <p>
-        Current participants:{" "}
-        {game.game_participants.map((part) => part.name).join(", ")}
-      </p>
+    <div class="w-full h-full flex justify-center items-center">
+      <div class="w-full md:w-1/2 h-full md:h-64 flex flex-col md:justify-between border rounded">
+        <div className="m-8">
+          <h1 className="mx-4 mb-4">Game lobby</h1>
+          <p className="mx-4 my-2">
+            Game code: <strong>{game.token}</strong>
+          </p>
+          <p className="mx-4 my-2">
+            Current participants:{" "}
+            {game.game_participants.map((part) => part.name).join(", ")}
+            {game.game_participants.length === 0 && "None"}
+          </p>
 
-      {participant && (
-        <button
-          className="py-2 px-4 rounded border-solid border border-black"
-          onClick={() => nextRound({ game })}
-        >
-          Start game
-        </button>
-      )}
+          {participant && (
+            <button
+              className="mx-4 my-2 py-2 px-4 rounded border-solid border border-black"
+              onClick={() => nextRound({ game })}
+            >
+              Start game
+            </button>
+          )}
 
-      {!participant && (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (name !== "") joinGame({ game, name, setParticipant });
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Name"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
-          <button
-            disabled={name === ""}
-            className="py-2 px-4 rounded border-solid border border-black"
-          >
-            Join game
-          </button>
-        </form>
-      )}
+          {!participant && (
+            <form
+              className="m-4"
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (name !== "") joinGame({ game, name, setParticipant });
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Name"
+                className="border rounded w-full mb-2 px-4 py-2"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+              <button
+                disabled={name === ""}
+                className="py-2 px-4 rounded border-solid border border-black"
+              >
+                Join game
+              </button>
+            </form>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
